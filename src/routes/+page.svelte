8958c5e6ef82cecab4d749d2c4970eefa0abc0d0
shiki-01 <0110shiki@gmail.com>
2024-06-16 19:09:56 +0900

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<main />
+
+<style>
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu,
+			Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+	}
+
+	main {
+		padding: 2em 1em 1em 1em;
+		text-align: center;
+		animation: fade 1s;
+		margin: 0 auto;
+	}
+</style>
